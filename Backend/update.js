a0164374daf068017ee02d5261dc18bc9315ad4d
cippero <gilwein@gmail.com>
2018-08-06 19:06:19 -0700

    deploying serverless setup to aws

---
+++ Backend/update.js
@@ -0,0 +1,30 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: "namad",
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            rideId: event.pathParameters.id
+        },
+        UpdateExpression: "SET content = :content, attachment = :attachment",
+        ExpressionAttributeValues: {
+            ":attachment": data.attachment ? data.attachment : null,
+            ":content": data.content ? data.content : null
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    try {
+        const result = await dynamoDbLib.call("update", params);
+        callback(null, success({
+            status: true
+        }));
+    } catch (e) {
+        callback(null, failure({
+            status: false
+        }));
+    }
+}
